7ba547b643336b664d9c54fad785b71899421aa1
Felix Fong <moongod101@hotmail.com>
2019-12-02 14:37:30 +0800

    Using Prettier to format code

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,25 @@
 <script>
-	const DB = 'https://www.jsonstore.io/8ee7336db06b263a98141280ccc41fadaf4b65db7b3a12d832631624c230a441';
-	const imgurClientID = 'df14151b1226e69';
-	
-	import DBStatus from './DBStatus.svelte';
-	import SaveNewView from './SaveNewView.svelte';
-	import ImageList from './ImageListing.svelte';
-	import { imagesStore } from './store.js';
-	
-	let isDBConnected = false;
-	
-	function onDBConnectionUpdate(e) {
-		const ok = e.detail.ok;
-		const payload = e.detail.payload;
-		isDBConnected = ok;
-	}
-	
+    const DB =
+        'https://www.jsonstore.io/8ee7336db06b263a98141280ccc41fadaf4b65db7b3a12d832631624c230a441';
+    const clientID = 'df14151b1226e69';
+
+    import DBStatus from './DBStatus.svelte';
+    import SaveNewView from './SaveNewView.svelte';
+    import ImageList from './ImageListing.svelte';
+    import { imagesStore } from './store.js';
+
+    let isDBConnected = false;
+
+    function onDBConnectionUpdate(e) {
+        const ok = e.detail.ok;
+        const payload = e.detail.payload;
+        isDBConnected = ok;
+    }
 </script>
 
-<DBStatus
-	DB={DB}
-  on:db_status_update={onDBConnectionUpdate}
-/>
+<DBStatus {DB} on:db_status_update={onDBConnectionUpdate} />
 
 {#if isDBConnected}
-	<SaveNewView DB={DB} skip_saving={false} clientID={imgurClientID} />
-	<ImageList />
+    <SaveNewView {DB} {clientID} skip_saving={false} />
+    <ImageList />
 {/if}
--- src/SaveNewView.svelte
+++ src/SaveNewView.svelte
@@ -1,90 +1,126 @@
 <script>
-	
-	import { imagesStore } from './store.js';
-	
-	export let skip_saving = false;
-	export let DB;
-	export let clientID;
-	
-	let uploading = false;
-	
-	let images = [];
-	
-	imagesStore.subscribe(imagesFromStore => {
-		return images = imagesFromStore || []
-	});
-	
-	async function encodeImage(file) {
-		return new Promise((resolve, reject) => {
-			let reader = new FileReader();
-			reader.onloadend = () => resolve(reader.result);
-			reader.onerror = err => reject(err);
-			reader.readAsDataURL(file);
-		});
-	}
-	
-	async function saveFileToDB(imgurURL) {
-		images.push({
-			date: new Date(),
-			image: imgurURL
-		});
-		const res = await fetch(DB, {
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(images),
-		});
-		console.log(images)
-		imagesStore.update(imagesFromStore => imagesFromStore = images);
-		return res;
-	}
-	
-	async function uploadFile(file) {
-		try {
-			if(skip_saving === false && file) {
-				uploading = true;
-				// const encedImage = await encodeImage(file);
-				const form = new FormData();
-				
-				form.append('image', file);
-				
-				const imageUploadRes = await fetch('https://api.imgur.com/3/image', {
-					method: 'POST',
-					headers: {
-						Authorization: `Client-ID ${clientID}` ,
-						Accept: 'application/json',
-					},
-					body: form,
-				});
-				uploading = false;
-				const payload = await imageUploadRes.json();
-				await saveFileToDB(payload.data.link);
-			} else {
-				console.log('Flag "skip_saving" is enabled or empty file selected');
-				return true;
-			}
-		} catch (err) {
-			console.error('Fail to upload new image or process image');
-			console.error(err);
-		}
-	}
-	
-	async function uploadHandler(e) {
-		const file = e.target.files[0];
-		try {
-			const res = await uploadFile(file);
-		} catch (err) {
-			console.error('Fail to upload new image');
-			console.error(err);
-		}
-	}
+    import { imagesStore } from './store.js';
+
+    export let skip_saving = false;
+    export let DB;
+    export let clientID;
+
+    let uploading = false;
+
+    let images = [];
+
+    imagesStore.subscribe(imagesFromStore => {
+        return (images = imagesFromStore || []);
+    });
+
+    async function encodeImage(file) {
+        return new Promise((resolve, reject) => {
+            let reader = new FileReader();
+            reader.onloadend = () => resolve(reader.result);
+            reader.onerror = err => reject(err);
+            reader.readAsDataURL(file);
+        });
+    }
+
+    async function saveFileToDB(imgurURL) {
+        images.push({
+            date: new Date(),
+            image: imgurURL,
+        });
+        const res = await fetch(DB, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(images),
+        });
+        console.log(images);
+        imagesStore.update(imagesFromStore => (imagesFromStore = images));
+        return res;
+    }
+
+    async function uploadFile(file) {
+        try {
+            if (skip_saving === false && file) {
+                uploading = true;
+                // const encedImage = await encodeImage(file);
+                const form = new FormData();
+
+                form.append('image', file);
+
+                const imageUploadRes = await fetch(
+                    'https://api.imgur.com/3/image',
+                    {
+                        method: 'POST',
+                        headers: {
+                            Authorization: `Client-ID ${clientID}`,
+                            Accept: 'application/json',
+                        },
+                        body: form,
+                    },
+                );
+                uploading = false;
+                const payload = await imageUploadRes.json();
+                debugger;
+                await saveFileToDB(payload.data.link);
+            } else {
+                console.log(
+                    'Flag "skip_saving" is enabled or empty file selected',
+                );
+                return true;
+            }
+        } catch (err) {
+            console.error('Fail to upload new image or process image');
+            console.error(err);
+        }
+    }
+
+    async function uploadHandler(e) {
+        const file = e.target.files[0];
+        try {
+            const res = await uploadFile(file);
+        } catch (err) {
+            console.error('Fail to upload new image');
+            console.error(err);
+        }
+    }
+
+    window.addEventListener('paste', e => {
+        const items = (event.clipboardData || event.originalEvent.clipboardData)
+            .items;
+        for (const index in items) {
+            var item = items[index];
+            if (item.kind === 'file') {
+                var blob = item.getAsFile();
+                uploadFile(blob);
+            }
+        }
+    });
 </script>
 
+<style>
+    .upload_container {
+        width: 100%;
+        position: relative;
+        display: flex;
+        justify-content: center;
+    }
+
+    #uploadbox {
+        width: 40em;
+        height: 30em;
+        border: 1px dotted #161616;
+    }
+</style>
+
 {#if uploading}
-	<p>
-		Uploading
-	</p>
+    <p>Uploading</p>
 {/if}
 
-<input type="file" accept="image/*" on:change={uploadHandler} />
+<div class="upload_container">
+    <input
+        type="file"
+        accept="image/*"
+        on:change={uploadHandler}
+        id="uploadbox" />
+</div>
